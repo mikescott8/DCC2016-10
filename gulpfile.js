5968b3ed8e030d86b7a74d0b673f421adebed64c
Mike Hamilton <mike.hamilton@neudesic.com>
2016-10-05 08:36:57 -0700

    start html tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp  = require('gulp');
 var util = require('gulp-util');
 var nodemon = require('gulp-nodemon');
+var clean = require('gulp-clean');
 
 // create a default task and just log a message
 gulp.task('default', function() {
@@ -10,4 +11,14 @@ gulp.task('default', function() {
 
 gulp.task('server:start', function () {
   nodemon( { script: './server.js' } );
+});
+
+gulp.task('cleanhtml', function () {
+  gulp.src(['public/*','!readme.md'], {read:false})
+  .pipe(clean());
+});
+
+gulp.task('html', 'cleanhtml', function  () {
+  gulp.src('source/**/*.html')
+  .pipe(gulp.dest('public'));
 });
